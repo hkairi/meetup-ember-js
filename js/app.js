4e839c22a7a1c3f781900fe6d7f9284611e03719
Hassane Moustapha <hassanemoustapha@gmail.com>
2013-11-06 15:12:49 +0000

    removed unused code*

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ Meetup.FacebookImageComponent = Ember.Component.extend({
 
   facebook_image: function(){
     var fb_id = this.get('fb_id');
-    console.log('..');
     return "https://graph.facebook.com/"+ fb_id +"/picture?type=large" 
   }.property('fb_id')
 
